99f1c5768519722f255d8e5941de0aa9c0ade9ce
Rob Thijssen <rthijssen@gmail.com>
2019-06-25 13:32:11 +0300

    historical data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate chrono;
+extern crate glob;
 extern crate reqwest;
 extern crate select;
 
@@ -6,8 +8,11 @@ extern crate serde_derive;
 extern crate serde_json;
 extern crate serde_yaml;
 
+use chrono::NaiveDate;
+use glob::glob;
+
 use select::document::Document;
-use select::predicate::{Class, Name, Predicate};
+use select::predicate::{Attr, Class, Name, Predicate};
 
 use std::collections::BTreeMap;
 use std::fs;
@@ -22,17 +27,34 @@ macro_rules! btreemap {
 }
 
 fn main() {
-  coin_market_cap("https://coinmarketcap.com/all/views/all/", "/home/rob/git/grenade/crypto-market-data");
+  let path = "/home/rob/git/grenade/crypto-market-data";
+  //coin_market_cap_all_today("https://coinmarketcap.com/all/views/all/", &path);
+  /*
+  if std::path::Path::new(&format!("{}/history", path)).exists() {
+    fs::remove_dir_all(format!("{}/history", path)).expect("unable to delete directory");
+  }
+  */
+  for file in glob(&format!("{}/today/json/*.json", &path)).expect("unable to interpret glob pattern") {
+    let id = file.as_ref().unwrap().file_stem().unwrap().to_str().unwrap();
+    if std::path::Path::new(&format!("{}/history/json/{}.json", &path, &id)).exists() {
+      println!("skipping history for {}",  &id);
+    } else {
+      coin_market_cap_history(&id, &format!("https://coinmarketcap.com/currencies/{}/historical-data/?start=20130428&end=20190625", &id), &path);
+      std::thread::sleep(std::time::Duration::from_secs(6));
+    }
+  }
 }
 
-fn coin_market_cap(url: &str, path: &str) {
-  for format in vec!["json", "yaml"].iter() {
-    fs::remove_dir_all(format!("{}/{}", path, format)).expect("unable to delete directory");
-    fs::create_dir_all(format!("{}/{}", path, format)).expect("unable to create directory");
+fn coin_market_cap_all_today(url: &str, path: &str) {
+  if std::path::Path::new(&format!("{}/today", path)).exists() {
+    fs::remove_dir_all(format!("{}/today", path)).expect("unable to delete directory");
   }
-  let resp = reqwest::get(url).unwrap();
-  assert!(resp.status().is_success());
-  let document = Document::from_read(resp).unwrap();
+  for format in vec!["json", "yaml"].iter() {
+    fs::create_dir_all(format!("{}/today/{}", path, format)).expect("unable to create directory");
+  }
+  let response = reqwest::get(url).unwrap();
+  assert!(response.status().is_success());
+  let document = Document::from_read(response).unwrap();
 
   let mut all = Vec::new();
 
@@ -61,30 +83,135 @@ fn coin_market_cap(url: &str, path: &str) {
     };
 
     let json_crypto = serde_json::to_string_pretty(&crypto).unwrap();
-    fs::write(format!("{}/json/{}.json", path, crypto.id), &json_crypto).expect("unable to write json file");
-    //println!("{}",  json_crypto);
+    let json_crypto_path = format!("{}/today/json/{}.json", path, crypto.id);
+    fs::write(&json_crypto_path, &json_crypto).expect("unable to write json file");
+    println!("{} updated",  json_crypto_path);
 
     let yaml_crypto = serde_yaml::to_string(&crypto).unwrap();
-    fs::write(format!("{}/yaml/{}.yaml", path, crypto.id), &yaml_crypto).expect("unable to write yaml file");
-    //println!("{}",  yaml_crypto);
+    let yaml_crypto_path = format!("{}/today/yaml/{}.yaml", path, crypto.id);
+    fs::write(&yaml_crypto_path, &yaml_crypto).expect("unable to write yaml file");
+    println!("{} updated",  yaml_crypto_path);
 
     all.push(crypto);
   }
+  /*
+  write_to_file(&all, &format!("{}/today/all.json", path));
+  write_to_file(&all, &format!("{}/today/all.yaml", path));
+
+  write_to_file(&all[..10], &format!("{}/today/top-10.json", path));
+  write_to_file(&all[..10], &format!("{}/today/top-10.yaml", path));
+
+  write_to_file(&all[..10], &format!("{}/today/top-10.json", path));
+  write_to_file(&all[..10], &format!("{}/today/top-10.yaml", path));
+  */
+
   let json_all = serde_json::to_string_pretty(&all).unwrap();
-  fs::write(format!("{}/all.json", path), &json_all).expect("unable to write json file");
+  let json_all_path = format!("{}/today/all.json", path);
+  fs::write(&json_all_path, &json_all).expect("unable to write json file");
+  println!("{} updated",  json_all_path);
+  
   let yaml_all = serde_yaml::to_string(&all).unwrap();
-  fs::write(format!("{}/all.yaml", path), &yaml_all).expect("unable to write yaml file");
+  let yaml_all_path = format!("{}/today/all.yaml", path);
+  fs::write(&yaml_all_path, &yaml_all).expect("unable to write yaml file");
+  println!("{} updated",  yaml_all_path);
 
   let json_top_10 = serde_json::to_string_pretty(&all[..10]).unwrap();
-  fs::write(format!("{}/top-10.json", path), &json_top_10).expect("unable to write json file");
+  let json_top_10_path = format!("{}/today/top-10.json", path);
+  fs::write(&json_top_10_path, &json_top_10).expect("unable to write json file");
+  println!("{} updated",  json_top_10_path);
+  
   let yaml_top_10 = serde_yaml::to_string(&all[..10]).unwrap();
-  fs::write(format!("{}/top-10.yaml", path), &yaml_top_10).expect("unable to write yaml file");
+  let yaml_top_10_path = format!("{}/today/top-10.yaml", path);
+  fs::write(&yaml_top_10_path, &yaml_top_10).expect("unable to write yaml file");
+  println!("{} updated",  yaml_top_10_path);
 
   let json_top_100 = serde_json::to_string_pretty(&all[..100]).unwrap();
-  fs::write(format!("{}/top-100.json", path), &json_top_100).expect("unable to write json file");
+  let json_top_100_path = format!("{}/today/top-100.json", path);
+  fs::write(&json_top_100_path, &json_top_100).expect("unable to write json file");
+  println!("{} updated",  json_top_100_path);
+  
   let yaml_top_100 = serde_yaml::to_string(&all[..100]).unwrap();
-  fs::write(format!("{}/top-100.yaml", path), &yaml_top_100).expect("unable to write yaml file");
+  let yaml_top_100_path = format!("{}/today/top-100.yaml", path);
+  fs::write(&yaml_top_100_path, &yaml_top_100).expect("unable to write yaml file");
+  println!("{} updated",  yaml_top_100_path);
 }
+
+fn coin_market_cap_history(id: &str, url: &str, path: &str) {
+  for format in vec!["json", "yaml"].iter() {
+    fs::create_dir_all(format!("{}/history/{}", path, format)).expect("unable to create directory");
+  }
+  let response = reqwest::get(url).unwrap();
+  if response.status().is_success() {
+    let document = Document::from_read(response).unwrap();
+
+    let mut history = Vec::new();
+
+    let keys = vec!["date_raw", "open", "high", "low", "close", "volume", "market_cap"];
+    for row in document.find(Attr("id", "historical-data").descendant(Name("tbody").descendant(Name("tr")))) {
+      let date_raw = row.find(Class("text-left")).next().unwrap().text();
+      let date_parsed = NaiveDate::parse_from_str(&date_raw, "%b %d, %Y").expect("unable to parse date");
+
+      let mut props = BTreeMap::new();
+      let mut cell_index = 0;
+      for cell in row.find(Name("td")) {
+        match cell_index {
+          0 => {
+            props.insert(keys[cell_index].to_string(), Value {
+              as_string: date_parsed.to_string(),
+              as_formatted_string: date_raw.to_string(),
+            });
+          }
+          _ => {
+            props.insert(keys[cell_index].to_string(), Value {
+              as_string: cell.attr("data-format-value").unwrap().to_string(),
+              as_formatted_string: cell.text().to_string(),
+            });
+          }
+        }
+        cell_index += 1;
+      }
+      history.push(props);
+    }
+    println!("{} updated: {} - {}", id, last(&history).unwrap()["date_raw"].as_string, first(&history).unwrap()["date_raw"].as_string);
+
+    let json_history = serde_json::to_string_pretty(&history).unwrap();
+    let json_history_path = format!("{}/history/json/{}.json", path, id);
+    fs::write(&json_history_path, &json_history).expect("unable to write json file");
+    println!("{} updated",  json_history_path);
+
+    let yaml_history = serde_yaml::to_string(&history).unwrap();
+    let yaml_history_path = format!("{}/history/yaml/{}.yaml", path, id);
+    fs::write(&yaml_history_path, &yaml_history).expect("unable to write yaml file");
+    println!("{} updated",  yaml_history_path);
+  } else {
+    println!("failed to fetch {}. request status: {:?}",  &url, response.status());
+    std::thread::sleep(std::time::Duration::from_secs(60));
+  }
+}
+
+fn first<T>(v: &Vec<T>) -> Option<&T> {
+    v.first()
+}
+
+fn last<T>(v: &Vec<T>) -> Option<&T> {
+    v.last()
+}
+
+/*
+fn write_to_file(content: &str, path: &str) {
+  match &path[path.len-4..] {
+    "json" => {
+      let json = serde_json::to_string_pretty(content).unwrap();
+      fs::write(path, &json).expect("unable to write json file");
+    }
+    "yaml" => {
+      let yaml = serde_yaml::to_string(content).unwrap();
+      fs::write(path, &yaml).expect("unable to write yaml file");
+    }
+    println!("{} written",  path);
+  }
+}
+*/
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all="camelCase")]
@@ -99,4 +226,11 @@ struct Crypto {
   supply: BTreeMap<String, String>,
   change: BTreeMap<String, String>,
   is_mineable: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+struct Value {
+  as_string: String,
+  as_formatted_string: String,
 }
